ce4a8957e78b3a3757bccd40269fc9dba5a5ca08
muthur <muthunagaswamy@gmail.com>
2015-06-18 10:08:56 -0700

    * Added html template for the address and user-info component

--- app/components/address-input.js
+++ app/components/address-input.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save: function () {
+      //Handler for save button click
+    },
+    cancel: function () {
+      //Handler for cancel button click
+    }
+  }
 });
